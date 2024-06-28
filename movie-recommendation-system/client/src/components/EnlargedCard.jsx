4233d317ebef60485df51ca28cc2acb3c4ab0d2f
adwait58 <adwait58@students.cs.ubc.ca>
2024-06-28 12:58:31 -0700

    add feeling lucky functionality

---
+++ movie-recommendation-system/client/src/components/EnlargedCard.jsx
@@ -0,0 +1,21 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const EnlargedCard = ({ movie, onClose }) => {
+  console.log("Movie data in EnlargedCard:", movie);
+  return (
+    <Card sx={{ color: "white" }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {movie.title}
+        </Typography>
+        <Typography variant="body2">{movie.overview}</Typography>
+        <Button variant="outlined" onClick={onClose}>
+          Close
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default EnlargedCard;
